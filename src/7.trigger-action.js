11f9712f85a6b3353144cf2ae386385dce0fd1ef
zhentian-wan <answer881215@gmail.com>
2020-01-19 14:28:33 +0200

    trigger action

---
+++ src/7.trigger-action.js
@@ -0,0 +1,23 @@
+const { Machine, interpret, send } = require("xstate");
+
+const echoMachine = Machine({
+  id: "echo",
+  initial: "listening",
+  states: {
+    listening: {
+      on: {
+        SPEAK: {
+          actions: send("ECHO") // trigger echo action
+        },
+        ECHO: {
+          actions: () => {
+            console.log("echo is called");
+          }
+        }
+      }
+    }
+  }
+});
+
+const service = interpret(echoMachine).start();
+service.send("SPEAK");
